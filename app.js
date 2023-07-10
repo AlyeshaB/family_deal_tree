2f7f026501da780aaa38f5994ce8ef18dc6cdd32
40379096 <abartley01@qub.ac.uk>
2023-07-10 17:06:45 +0100

    more routes added

--- app.js
+++ app.js
@@ -24,38 +24,83 @@ app.get("/", (req, res) => {
 
 app.get("/deals", (req, res) => {
   const title = "Bargains";
-  let sql = `SELECT deal_id, title, description, deal_uri, image_uri, post_date, price, original_price, user_id, merchant_id, location_id FROM deal`;
-  connection.query(sql, (err, dealData) => {
+  let sql = `SELECT * FROM deal`;
+  connection.query(sql, (err, data) => {
     if (err) {
       throw err;
     }
-    res.render("deals", { tdata: title, dealData });
+    res.render("deals", { tdata: title, deals: data });
+  });
+});
+
+app.get("/deals/:deal_id", (req, res) => {
+  const dealId = req.params.deal_id;
+
+  // Use dealId to fetch the specific deal's details from the database
+  const sql = "SELECT * FROM deal WHERE deal_id = ?";
+
+  connection.query(sql, [dealId], (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    // Check if results were returned
+    if (data.length === 0) {
+      return res.status(404).send("No deal found with this ID");
+    }
+    const title = data[0].title;
+    // Render the deal detail page with the specific deal's details
+    res.render("deal_result", { tdata: title, deals: data });
+    console.log(data);
   });
 });
 
 app.get("/vouchers", (req, res) => {
   const title = "Vouchers";
-  res.render("vouchers", { tdata: title });
+  let sql =
+    "SELECT voucher.voucher_id, voucher.title, voucher.discount, voucher.merchant_id, merchant.* FROM voucher JOIN merchant ON voucher.merchant_id = merchant.merchant_id";
+  connection.query(sql, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.render("vouchers", { tdata: title, vouchers: data });
+  });
 });
 
 app.get("/merchants", (req, res) => {
   const title = "Merchants";
-  connection.query(
-    "SELECT * FROM merchant ORDER BY merchant_name ASC",
-    (err, results) => {
-      if (err) {
-        throw err;
-      } else {
-        // render the page and pass the results
-        res.render("merchants", { tdata: title, merchants: results });
-      }
+  let sql = "SELECT * FROM merchant ORDER BY merchant_name ASC";
+  connection.query(sql, (err, results) => {
+    if (err) {
+      throw err;
+    } else {
+      // render the page and pass the results
+      res.render("merchants", { tdata: title, merchants: results });
     }
-  );
+  });
 });
 
 app.get("/categories", (req, res) => {
   const title = "Categories";
   res.render("categories", { tdata: title });
+});
+
+app.get("/categories/:categorySlug", (req, res) => {
+  const title = "Bargains";
+  const categorySlug = req.params.categorySlug;
+  const sql = `SELECT deal.*, category.category_name 
+  FROM deal 
+  JOIN deal_category ON deal.deal_id = deal_category.deal_id 
+  JOIN category ON deal_category.category_id = category.category_id 
+  WHERE category.slug = ?`;
+
+  connection.query(sql, [categorySlug], (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.render("deals", { tdata: title, deals: data });
+    console.log(data);
+  });
 });
 
 app.get("/register", (req, res) => {
